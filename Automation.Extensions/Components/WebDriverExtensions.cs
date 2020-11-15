7651d5bf60540ac8a3b50eefbc31c61da6d440c0
Tom Goldman <tomgoldman11@gmail.com>
2020-11-15 15:01:18 +0200

    Adding Useful Extension methods to the webDriver

---
+++ Automation.Extensions/Components/WebDriverExtensions.cs
@@ -0,0 +1,136 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Automation.Extensions.Components
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebDriver GoToUrl(this IWebDriver driver, string url)
+        {
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            return driver; 
+        }
+        //Gives us default timeout
+        public static IWebElement GetElement(this IWebDriver driver, By by) => GetElement(driver, by, TimeSpan.FromSeconds(15)); 
+     
+        public static IWebElement GetElement(this IWebDriver driver, By by, TimeSpan timeout)
+        {
+            var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
+            return wait.Until(d => d.FindElement(by));
+        }
+
+        public static SelectElement AsSelect(this IWebElement element) => new SelectElement(element);
+
+        public static ReadOnlyCollection<IWebElement> GetElements(this IWebDriver driver, By by) => GetElements(driver, by, TimeSpan.FromSeconds(15));
+
+        public static ReadOnlyCollection<IWebElement> GetElements(this IWebDriver driver, By by, TimeSpan timeout)
+        {
+            var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
+            return wait.Until(d => d.FindElements(by));
+        }
+
+        public static IWebElement GetVisibleElement(this IWebDriver driver, By by) => GetVisibleElement(driver, by, TimeSpan.FromSeconds(15));
+        
+        public static IWebElement GetVisibleElement(this IWebDriver driver, By by, TimeSpan timeout)
+        {
+            var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
+            return wait.Until(d =>
+            {
+                var element = d.FindElement(by);
+                return element.Displayed ? element : null;
+            });
+        }
+
+        public static ReadOnlyCollection<IWebElement> GetVisibleElements(this IWebDriver driver, By by) => GetVisibleElements(driver, by, TimeSpan.FromSeconds(15));
+
+        public static ReadOnlyCollection<IWebElement> GetVisibleElements(this IWebDriver driver, By by, TimeSpan timeout)
+        {
+            var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
+            return wait.Until(d =>
+            {
+                var elements = d.FindElements(by).Where(e => e.Displayed).ToList();
+                return new ReadOnlyCollection<IWebElement>(elements);
+            });
+        }
+
+        public static IWebElement GetEnabledElement(this IWebDriver driver, By by) => GetEnabledElement(driver, by, TimeSpan.FromSeconds(15));
+
+        public static IWebElement GetEnabledElement(this IWebDriver driver, By by, TimeSpan timeout)
+        {
+            var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
+            return wait.Until(d =>
+            {
+                var element = d.FindElement(by);
+                return element.Enabled ? element : null;
+            });
+        }
+
+        public static IWebDriver VerticalWindowScroll(this IWebDriver driver, int scrollAmount)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript($"window.scroll(0,{scrollAmount});");
+            return driver;
+        }
+
+        public static Actions Actions(this IWebElement element)
+        {
+            var driver = ((IWrapsDriver)element).WrappedDriver;
+            var actions = new Actions(driver);
+            return actions.MoveToElement(element);
+        }
+
+        public static IWebElement ForceClick(this IWebElement element)
+        {
+            var driver = ((IWrapsDriver)element).WrappedDriver;
+            var executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].click();", element); //Performing click using JS, useful when you can't use actual click
+            return element;
+        }
+
+        public static IWebElement SendKeys(this IWebElement element, string text, int interval)
+        {
+            foreach (char c in text)
+            {
+                element.SendKeys($"{c}");
+                Thread.Sleep(interval);
+            }
+            return element;
+        }
+
+        public static IWebElement ForceClear(this IWebElement element)
+        {
+            var value = element.GetAttribute("value");
+            element.SendKeys(Keys.End);
+            for (int i = 0; i < value.Length; i++)
+            {
+                element.SendKeys(Keys.Backspace);
+                Thread.Sleep(50);
+            }
+            return element; 
+        }
+
+        public static IWebDriver SubmitForm(this IWebDriver driver, int index)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript($"document.forms[{index}].submit();");
+            return driver;
+        }
+
+        public static IWebDriver SubmitForm(this IWebDriver driver, string id)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript($"document.forms[\"{id}\"].submit();");
+            return driver;
+        }
+
+    }//End Class 
+
+}//End Namespace
+ 
--- Automation.Testing/SeleniumSamples.cs
+++ Automation.Testing/SeleniumSamples.cs
@@ -6,6 +6,8 @@ using OpenQA.Selenium.Support.UI;
 using System.Threading;
 using Automation.Extensions.Components;
 using Automation.Extensions.Contracts;
+using OpenQA.Selenium.Edge;
+using System.Drawing;
 
 namespace Automation.Testing
 {
@@ -48,6 +50,7 @@ namespace Automation.Testing
             driver.Navigate().GoToUrl("https://google.com");
             driver.Dispose();
         }
+
         [TestMethod]
         public void WebDriverFactorySample()
         {
@@ -58,5 +61,140 @@ namespace Automation.Testing
             Thread.Sleep(2000);
             driver.Dispose();
         }
-    }
-}
+
+        [TestMethod]
+        public void GoToUrlSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net");
+            Thread.Sleep(2000);
+            driver.Dispose(); 
+        }
+
+        [TestMethod]
+        public void GetElementSample() //GetElement is Extension method we created in order to have the capability to wait for an element to appear
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://gravitymvctestapplication.azurewebsites.net");
+            driver.GetElement(By.XPath("//a[.='Students']")).Click();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void AsSelectSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();  
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Course");
+            driver.FindElement(By.XPath("//select[@id='SelectedDepartment']")).AsSelect().SelectByValue("4");
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void GetElementsSample() 
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net");
+            var elements = driver.GetElements(By.XPath("//ul/li")); 
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void GetVisibleElementSample() //Clicks only if the element is visible (GetElement could try to click an existing but not visible element which will cause an exception)
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net");
+            driver.GetVisibleElement(By.XPath("//a[.='Students']")).Click();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void GetVisibleElementsSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net");
+            var elements = driver.GetVisibleElements(By.XPath("//ul/li"));
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void GetEnabledElementSample() 
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            driver.GetEnabledElement(By.XPath("//input[@id='SearchString']")).SendKeys("Hello");
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void VerticalWindowScrollSample() 
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            driver.Manage().Window.Size = new Size(100, 350);
+            driver.VerticalWindowScroll(1000); //Scroll to point 1000 ( not 1000 pixels )
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void ActionsSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net");
+            driver.GetVisibleElement(By.XPath("//a[.='Students']")).Actions().Click().Build().Perform(); //Last point will be the element itself because we moved to it inside Actions method.
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void ForceClickSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net");
+            driver.GetVisibleElement(By.XPath("//a[.='Students']")).ForceClick();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void SendKeysIntervalSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            driver.GetEnabledElement(By.XPath("//input[@id='SearchString']")).SendKeys("Hello", 1000);
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void ForceClearSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            driver.GetEnabledElement(By.XPath("//input[@id='SearchString']")).SendKeys("Hello",0).SendKeys(Keys.Home,0).ForceClear();
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+        [TestMethod]
+        public void SubmitFormSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = @"C:\web-drivers" }).Get();
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/Student");
+            driver.GetEnabledElement(By.XPath("//input[@id='SearchString']")).SendKeys("Alexander");
+            driver.SubmitForm(0);
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
+
+    }//End Class
+
+}//End Namespace

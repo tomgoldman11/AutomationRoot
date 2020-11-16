04203f146d3471bd80c712642c10e336388a5415
Tom Goldman <tomgoldman11@gmail.com>
2020-11-16 19:25:07 +0200

    indentation and creating nuget package on build

--- Automation.Extensions/Components/WebDriverExtensions.cs
+++ Automation.Extensions/Components/WebDriverExtensions.cs
@@ -22,25 +22,19 @@ namespace Automation.Extensions.Components
         }
         //Gives us default timeout
         public static IWebElement GetElement(this IWebDriver driver, By by) => GetElement(driver, by, TimeSpan.FromSeconds(15)); 
-     
         public static IWebElement GetElement(this IWebDriver driver, By by, TimeSpan timeout)
         {
             var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
             return wait.Until(d => d.FindElement(by));
         }
-
         public static SelectElement AsSelect(this IWebElement element) => new SelectElement(element);
-
         public static ReadOnlyCollection<IWebElement> GetElements(this IWebDriver driver, By by) => GetElements(driver, by, TimeSpan.FromSeconds(15));
-
         public static ReadOnlyCollection<IWebElement> GetElements(this IWebDriver driver, By by, TimeSpan timeout)
         {
             var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
             return wait.Until(d => d.FindElements(by));
         }
-
         public static IWebElement GetVisibleElement(this IWebDriver driver, By by) => GetVisibleElement(driver, by, TimeSpan.FromSeconds(15));
-        
         public static IWebElement GetVisibleElement(this IWebDriver driver, By by, TimeSpan timeout)
         {
             var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
@@ -50,9 +44,7 @@ namespace Automation.Extensions.Components
                 return element.Displayed ? element : null;
             });
         }
-
         public static ReadOnlyCollection<IWebElement> GetVisibleElements(this IWebDriver driver, By by) => GetVisibleElements(driver, by, TimeSpan.FromSeconds(15));
-
         public static ReadOnlyCollection<IWebElement> GetVisibleElements(this IWebDriver driver, By by, TimeSpan timeout)
         {
             var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
@@ -62,9 +54,7 @@ namespace Automation.Extensions.Components
                 return new ReadOnlyCollection<IWebElement>(elements);
             });
         }
-
         public static IWebElement GetEnabledElement(this IWebDriver driver, By by) => GetEnabledElement(driver, by, TimeSpan.FromSeconds(15));
-
         public static IWebElement GetEnabledElement(this IWebDriver driver, By by, TimeSpan timeout)
         {
             var wait = new WebDriverWait(driver, timeout); // reapeat until element is found or reaching timeout
@@ -74,20 +64,17 @@ namespace Automation.Extensions.Components
                 return element.Enabled ? element : null;
             });
         }
-
         public static IWebDriver VerticalWindowScroll(this IWebDriver driver, int scrollAmount)
         {
             ((IJavaScriptExecutor)driver).ExecuteScript($"window.scroll(0,{scrollAmount});");
             return driver;
         }
-
         public static Actions Actions(this IWebElement element)
         {
             var driver = ((IWrapsDriver)element).WrappedDriver;
             var actions = new Actions(driver);
             return actions.MoveToElement(element);
         }
-
         public static IWebElement ForceClick(this IWebElement element)
         {
             var driver = ((IWrapsDriver)element).WrappedDriver;
@@ -95,17 +82,15 @@ namespace Automation.Extensions.Components
             executor.ExecuteScript("arguments[0].click();", element); //Performing click using JS, useful when you can't use actual click
             return element;
         }
-
         public static IWebElement SendKeys(this IWebElement element, string text, int interval)
         {
             foreach (char c in text)
             {
-                element.SendKeys($"{c}");
+                element.SendKeys($"{c}"); //converting the char into string
                 Thread.Sleep(interval);
             }
             return element;
         }
-
         public static IWebElement ForceClear(this IWebElement element)
         {
             var value = element.GetAttribute("value");
@@ -117,13 +102,11 @@ namespace Automation.Extensions.Components
             }
             return element; 
         }
-
         public static IWebDriver SubmitForm(this IWebDriver driver, int index)
         {
             ((IJavaScriptExecutor)driver).ExecuteScript($"document.forms[{index}].submit();");
             return driver;
         }
-
         public static IWebDriver SubmitForm(this IWebDriver driver, string id)
         {
             ((IJavaScriptExecutor)driver).ExecuteScript($"document.forms[\"{id}\"].submit();");

12ea9b94be0d01652cd10817173cc3d744c96d5b
Tom Goldman <tomgoldman11@gmail.com>
2020-11-29 17:02:08 +0200

    Adding couple of base projects, adding simple TraceLogger and ILogger Interface

---
+++ Automation.Core/Logging/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Automation.Core.Logging
+{
+    public interface ILogger
+    {
+        void Debug(string message);
+        void Debug(string format, params object[] args);
+        void Debug(Exception exeception, string message);
+    }
+}
---
+++ Automation.Core/Logging/TraceLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Automation.Core.Logging
+{
+    public class TraceLogger : ILogger
+    {
+        public void Debug(string message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            Trace.TraceInformation(string.Format(format, args));
+        }
+
+        public void Debug(Exception exception, string message)
+        {
+            Trace.TraceError(message + " " + $"{exception}");
+        }
+    }
+}
+ 

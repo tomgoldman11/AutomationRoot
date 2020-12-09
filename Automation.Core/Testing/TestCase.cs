310c398b2e92c41471bb627fe600ce85ed86aacc
Tom Goldman <tomgoldman11@gmail.com>
2020-12-09 12:06:46 +0200

    Creating TestCases Class

---
+++ Automation.Core/Testing/TestCase.cs
@@ -0,0 +1,41 @@
+﻿using Automation.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automation.Core.Testing
+{
+    public abstract class TestCase
+    {
+        //fields
+        
+        private IDictionary<string, object> testParams;
+        private int attempts;
+        private ILogger logger;
+
+        // components
+        public abstract bool AutomationTest(IDictionary<string, object> testParams);
+        public TestCase Execute()
+        {
+            return this;
+        }
+
+        // configurations
+        public TestCase WithTestParams(IDictionary<string, object> testParams)
+        {
+            this.testParams = testParams;
+            return this;
+        }
+        public TestCase WithNumberOfAttempts(int numberOfAttempts)
+        {
+            attempts = numberOfAttempts;
+            return this;
+        }
+        public TestCase WithLogger(ILogger logger)
+        {
+            this.logger = logger;
+            return this;
+        }
+    }
+}
+ 
